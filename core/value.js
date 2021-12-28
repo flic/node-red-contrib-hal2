996097b092073c8eab07c5aec33513304de92f97
Fredrik Furtenbach <fredrik@furtenbach.se>
2021-12-28 17:22:29 +0100

    Dynamic thing id in value node

--- core/value.js
+++ core/value.js
@@ -2,6 +2,7 @@ module.exports = function(RED) {
     function hal2Value(config) {
         RED.nodes.createNode(this,config);
         this.thing = config.thing;
+        this.thingType = config.thingType;
         this.item = config.item;
         this.outputValue = config.outputValue;
         this.outputType = config.outputType;
@@ -9,7 +10,27 @@ module.exports = function(RED) {
         var node = this;
 
         node.on('input', function(msg) {
-            var thing = RED.nodes.getNode(node.thing);
+            var thing;
+            if (node.thing == '0') {
+                if ((typeof msg.thing !== 'undefined') && (typeof msg.thing.id !== 'undefined')) {
+                    thing = RED.nodes.getNode(msg.thing.id);
+                    if (thing === null) {
+                        node.error("Can't find thing with id "+msg.thing.id);
+                        return;
+                    } else if (thing.type !== 'hal2Thing') {
+                        node.error("Node with id "+msg.thing.id+ " isn't a thing");
+                        return;                        
+                    } else if (thing.thingType.id !== node.thingType) {
+                        node.error("Node with id "+msg.thing.id+ " is of the wrong type");
+                        return;                         
+                    }
+                } else {
+                    node.error("thing.id missing from payload");
+                    return;
+                }
+            } else {
+                thing = RED.nodes.getNode(node.thing);
+            }
 
             if (!thing.state.hasOwnProperty(node.item)) {
                 // No value stored in item
