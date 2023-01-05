03fab5141d4cd394151cfd0da0dea2728833c840
Fredrik Furtenbach <fredrik@furtenbach.se>
2023-01-05 14:41:16 +0100

    remove configlibrary

--- resources/configlibrary.js
+++
@@ -1,8 +0,0 @@
-function isStatusItem(item) {
-    return ((item.type == 'both') || (item.type == 'status') || (item.type == 'loopback_both') || (item.id == '1'));
-}
-
-function isCommandItem(item) {
-    return ((item.type == 'both') || (item.type == 'command') || (item.type == 'loopback_both') || (item.type == 'loopback_command'));
-}
-
